40efc12cc85213b684e7f63aa5611453b6f91ef0
Shantamoy Guha <shantamoy@gmail.com>
2024-10-03 13:48:57 +0530

    Update area_calculation.m

--- area_calculation.m
+++ area_calculation.m
@@ -1,4 +1,4 @@
 clc
 clear all
-data = xlsread('Section.xlsx');
+data = xlsread('Section.xlsx'); %add your section data here
 [A, Q, D, V, W, h] = crosssection_area(data, 'slope',0.008, 'manning', 0.03);
